8d5d732ee9f6b80b230700b6faee206026ca3505
AmosNyaundi <amosnyaundi4@gmail.com>
2024-01-19 21:09:46 +0300

    new code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
@@ -12,7 +13,17 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            // console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            console.log(parsedBody);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
